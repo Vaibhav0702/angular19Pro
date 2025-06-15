45952e684d7ac4276ad0d99311636fa0237733e1
Vaibhav <vaibhavginnalwar@gmail.com>
2025-06-15 17:32:12 +0530

    create 1 page component (profile)

--- angualr19Pro/src/app/app.component.ts
+++ angualr19Pro/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { profileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , LoginComponent , SignupComponent , profileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angualr19Pro/src/app/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'app-profile',
+    template : '<h1> profile component </h1>',
+    styles : 'h1{color : red}'
+})
+
+
+export class profileComponent{
+
+
+
+}
